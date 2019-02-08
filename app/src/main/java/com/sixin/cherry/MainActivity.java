2abf84dab0ec3ffff0563a256ed5776e58ed7a8d
PhysicalOriginal <13645532096@163.com>
2019-02-08 16:44:38 +0800

    处理action_down事件

--- app/src/main/java/com/sixin/cherry/MainActivity.java
+++ app/src/main/java/com/sixin/cherry/MainActivity.java
@@ -2,7 +2,9 @@ package com.sixin.cherry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
--- library/src/main/java/com/sixin/library/LockView.java
+++ library/src/main/java/com/sixin/library/LockView.java
@@ -1,24 +1,35 @@
 package com.sixin.library;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.HapticFeedbackConstants;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LockView extends View {
 
     //todo 当通过纯java代码创建该view对象的时候，也能够实现UI效果
     //TODO lOG日志最后全部撤销
+    //TODO 提供get set方法 当提供这些方法后因为内部数据可能没有同步更新，会出现很多的bug.
     private static final String TAG = "LockView";
 
     //默认点的个数
     private static final int S_DEFAULT_DOT_COUNT = 3;
     //默认点被选中时的动画周期
     private static final int S_DEFAULT_DOT_SELECTED_ANIM_DURATION = 1500;
-
+    //todo 该变量暂时不作为属性向外抛
+    private static final float S_GRID_SCALE_FACTOR = 0.6f;
     /**
      * 点的个数，LockView中点的总数是mDotCount*mDotCount;
      * 注意mDotCount的个数被限制在大于0小于等于10的区间内，否则会抛出异常
@@ -29,6 +40,11 @@ public class LockView extends View {
      * 正常状态时点的大小
      * */
     private int mDotNormalSize;
+
+    /**
+     * 点被选中时的大小
+     * */
+    private int mDotSelectedSize;
 
     /**
      * 点被选中时的动画周期
@@ -62,14 +78,18 @@ public class LockView extends View {
     private float mGridHeight;
 
     /**
-     * 点的半径
+     * 触摸点的时候是否震动反馈
      * */
-    private float mRadius;
+    private boolean mFeedbackEnabled;
 
     /**
      * 点的画笔
      * */
     private Paint mDotPaint;
+
+    private Dot[][] mDots;
+
+    private List<ObjectAnimator> objectAnimators = new ArrayList<>();
 
     public LockView(Context context) {
         this(context, null);
@@ -93,22 +113,34 @@ public class LockView extends View {
         if (mDotNormalSize <= 0) {
             throw new IllegalArgumentException("mDotNormalSize must be >= 0");
         }
+        mDotSelectedSize = typedArray.getDimensionPixelSize(R.styleable.LockView_dotSelectedSize,getResources().getDimensionPixelSize(R.dimen.dotSelectedSize));
+        if (mDotSelectedSize <= 0) {
+            throw new IllegalArgumentException("mDotSelectedSize must be >= 0");
+        }
         mDotSelectedDuration = typedArray.getInteger(R.styleable.LockView_dotSelectedAnimDuration, S_DEFAULT_DOT_SELECTED_ANIM_DURATION);
         mDotNormalColor = typedArray.getColor(R.styleable.LockView_dotNormalColor, getResources().getColor(R.color.ColorDotNormal));
         mDotSelectedColor = typedArray.getColor(R.styleable.LockView_dotSelectedColor, getResources().getColor(R.color.ColorDotSelected));
         //todo 注意线条宽度比正常圆大的问题
         mPathWidth = typedArray.getDimensionPixelSize(R.styleable.LockView_pathWidth, getResources().getDimensionPixelSize(R.dimen.pathWidth));
         mColorWrong = typedArray.getColor(R.styleable.LockView_wrongColor, getResources().getColor(R.color.ColorWrong));
+        mFeedbackEnabled = typedArray.getBoolean(R.styleable.LockView_feedbackEnabled, true);
         typedArray.recycle();
     }
 
     private void initView() {
-        mRadius = mDotNormalSize / 2f;
 
         mDotPaint = new Paint();
         mDotPaint.setAntiAlias(true);
         mDotPaint.setDither(true);
         mDotPaint.setColor(mDotNormalColor);
+
+        float radius = mDotNormalSize/2f;
+        mDots = new Dot[mDotCount][mDotCount];
+        for (int i = 0; i < mDotCount; i++) {
+            for (int j = 0; j < mDotCount; j++) {
+                mDots[i][j] = new Dot(i,j,radius);
+            }
+        }
     }
 
     @Override
@@ -145,23 +177,193 @@ public class LockView extends View {
         if (mDotNormalSize > limitValue) {
             throw new IllegalArgumentException("Points are smaller than the shortest edge of a rectangle");
         }
+        //todo 该异常提示不够清晰，需要重新提示
+        if (mDotSelectedSize > limitValue) {
+            throw new IllegalArgumentException("Points are smaller than the shortest edge of a rectangle");
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        //todo canvas的save和restore   h5画布的学习
-        float offsetWidth = mGridWidth;
-        float offsetHeight = mGridHeight;
         for (int i = 0; i < mDotCount; i++) {
             for (int j = 0; j < mDotCount; j++) {
-                float cx = getPaddingStart() + mGridWidth/2 + j * offsetWidth;
-                float cy = getPaddingTop() + mGridHeight/2 + i * offsetHeight;
-                drawCircle(canvas, cx, cy);
+                float cx = getCx(j);
+                float cy = getCy(i);
+                drawCircle(canvas, cx, cy,mDots[i][j]);
             }
         }
     }
 
-    private void drawCircle(Canvas canvas, float cx, float cy) {
-        canvas.drawCircle(cx,cy,mRadius,mDotPaint);
+    private float getCx(int column) {
+        return getPaddingStart() + mGridWidth/2 + column * mGridWidth;
+    }
+
+    private float getCy(int row) {
+        return getPaddingTop() + mGridHeight/2 + row * mGridHeight;
+    }
+
+    private void drawCircle(Canvas canvas, float cx, float cy,Dot dot) {
+        if (dot != null) {
+            canvas.drawCircle(cx,cy,dot.getRadius(),mDotPaint);
+        }
+    }
+
+    //todo 考虑这个警告是否需要处理
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                handleActionDown(event);
+                return true;
+        }
+        return false;
+    }
+
+    private void handleActionDown(MotionEvent event) {
+        Dot dot = getTouchedDot(event.getX(), event.getY());
+        if (dot != null) {
+            startScaleAnimation(dot);
+            if (mFeedbackEnabled) {
+                performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY, HapticFeedbackConstants.FLAG_IGNORE_VIEW_SETTING|HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
+            }
+        }
+    }
+
+    private void startScaleAnimation(final Dot dot) {
+        if (dot != null) {
+            //todo 注意动画的内存泄漏问题
+            //todo 测试选中view与原view一样的大情况和与网格一样大的情况
+            float originRadius = mDotNormalSize/2f;
+            float selectedRadius = mDotSelectedSize / 2f;
+            ObjectAnimator scaleAnimator = ObjectAnimator
+                                                .ofFloat(dot,"radius",originRadius,selectedRadius,originRadius)
+                                                .setDuration(mDotSelectedDuration);
+            scaleAnimator.setInterpolator(new LinearOutSlowInInterpolator());
+            scaleAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    invalidate();
+                }
+            });
+
+            objectAnimators.add(scaleAnimator);
+
+            scaleAnimator.start();
+        }
+    }
+
+    /**
+     * 根据手指按下的坐标位置，确定触摸的具体Dot是哪一个
+     * @param x 手指按下的横坐标
+     * @param y 手指按下的纵坐标
+     * @return 点的对象，可能为空
+     * */
+    private Dot getTouchedDot(float x, float y) {
+        int row = getRow(y);
+        int column = getColumn(x);
+        if (row < 0 || column < 0) {
+            return null;
+        }
+
+        float cx = getCx(column);
+        float cy = getCy(row);
+        float responseAreaWidthHalf = (mGridWidth * S_GRID_SCALE_FACTOR)/2;
+        float responseAreaHeightHalf = (mGridHeight * S_GRID_SCALE_FACTOR)/2;
+        float radius = mDotNormalSize / 2f;
+        boolean isDotBeyondResponseArea = radius > Math.min(responseAreaHeightHalf, responseAreaWidthHalf);
+        if (isDotBeyondResponseArea) {
+            boolean xInArea = x >= (cx - radius) && x <= (cx + radius);
+            boolean yInArea = y >= (cy - radius) && y <= (cy + radius);
+            if (xInArea && yInArea) {
+                return mDots[row][column];
+            }else{
+                return null;
+            }
+        }else{
+            boolean xInArea = x >= (cx - responseAreaWidthHalf) && x <= (cx + responseAreaWidthHalf);
+            boolean yInArea = y >= (cy - responseAreaHeightHalf) && y <= (cy + responseAreaHeightHalf);
+            if (xInArea && yInArea) {
+                return mDots[row][column];
+            }else{
+                return null;
+            }
+        }
+    }
+
+    /**
+     * 根据触摸点的纵坐标确定触摸区域所在的行号
+     * @param y 触摸点的纵坐标
+     * @return 行号 默认返回-1,表示不在触摸区域内
+     * */
+    private int getRow(float y) {
+        int row = -1;
+        for (int i = 0; i < mDotCount; i++) {
+            if (y >= (getPaddingTop() + i * mGridHeight) && y <= (getPaddingTop() + (i + 1) * mGridHeight)) {
+                row = i;
+                break;
+            }
+        }
+        return row;
+    }
+
+    /**
+     * 根据触摸点的横坐标确定触摸区域所在的列号
+     * @param x 触摸点的横坐标
+     * @return 列号 默认返回-1,表示不在触摸区域内
+     * */
+    private int getColumn(float x){
+        int column = -1;
+        for (int i = 0; i < mDotCount; i++) {
+            if (x >= (getPaddingStart() + i * mGridWidth) && x <= (getPaddingStart() + (i + 1) * mGridWidth)) {
+                column = i;
+                break;
+            }
+        }
+        return column;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        //取消动画
+        for (ObjectAnimator objectAnimator : objectAnimators) {
+            if (objectAnimator != null && objectAnimator.isRunning()) {
+                objectAnimator.cancel();
+            }
+        }
+        //todo 考察在这其中还需要释放那些资源
+    }
+
+    /**
+     * lockView中点的抽象描述
+     * */
+    private static class Dot{
+
+        private int row;
+        private int column;
+        private float radius;
+
+        Dot(int row, int column,float radius) {
+            this.row = row;
+            this.column = column;
+            this.radius = radius;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+
+        public float getRadius() {
+            return radius;
+        }
+
+        public void setRadius(float radius) {
+            this.radius = radius;
+        }
     }
 }
